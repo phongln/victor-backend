cb17bee70333421507ac32bfab2f8708e6947e33
Vinh Pham <phamtanvinh.me@gmail.com>
2019-08-27 13:59:42 +0700

    add teardown session

--- api/__init__.py
+++ api/__init__.py
@@ -11,13 +11,11 @@ def create_app():
 
     with app.app_context():
         CORS(app)
-        from api.database import db, ma
-        db.init_app(app)
-        ma.init_app(app)
+        from api.database import init_datatabase
+        init_datatabase(app)
 
-        from api.resources import api, init_blueprint
-        api.init_app(app)
-        init_blueprint(app)
+        from api.resources import init_resources
+        init_resources(app)
 
     return app
 
--- api/database.py
+++ api/database.py
@@ -1,5 +1,5 @@
 from functools import wraps, partial
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
@@ -9,19 +9,23 @@ db = SQLAlchemy()
 ma = Marshmallow()
 
 
-def load_session():
-    Session = sessionmaker(bind=db.engine)
-
-    return Session()
-
-
 def get_session(func=None, *deco_args, **deco_kwargs):
     if func is None:
         return partial(get_session, *deco_args, **deco_kwargs)
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        session = load_session()
+        session = db.Session()
         return func(session, *args, **kwargs)
 
     return wrapper
+
+
+def init_datatabase(app):
+    db.init_app(app)
+    db.Session = scoped_session(sessionmaker(bind=db.engine))
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db.Session.remove()
+
+    ma.init_app(app)
