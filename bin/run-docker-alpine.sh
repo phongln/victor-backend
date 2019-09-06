2e9ca64af3daaf17b5c27c24b47e66940cf8c71a
vinhpham <phamtanvinh.me@gmail.com>
2019-09-07 03:27:12 +0700

    [2019-09-07 03:27] Vinhs-Mac.local vinhpham: dockerize all

---
+++ bin/run-docker-alpine.sh
@@ -0,0 +1,3 @@
+docker run -it --rm --name alpine \
+    --net backend-net \
+    alpine:latest
--- bin/run-docker-postgres.sh
+++ bin/run-docker-postgres.sh
@@ -1,4 +1,5 @@
 docker run -d --name postgres \
     -e POSTGRES_PASSWORD=Aa123456 \
+    --net backend-net \
     -p 5432:5432 \
     postgres
