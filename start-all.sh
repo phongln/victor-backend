351ccb4fb48ed987a0f981c161fef130ab5d31d4
vinhpham <phamtanvinh.me@gmail.com>
2019-09-10 22:33:38 +0700

    [2019-09-10 22:33] Vinhs-Mac.local vinhpham: resgiter backend-api

--- start-all.sh
+++ start-all.sh
@@ -11,3 +11,7 @@ docker-compose -p $PROJECT_NAME -f $DOCKER_COMPOSE_DIR/api.yml up -d --build
 docker-compose -p $PROJECT_NAME -f $DOCKER_COMPOSE_DIR/gateway.yml up -d --build
 docker-compose -p $PROJECT_NAME -f $DOCKER_COMPOSE_DIR/nginx-proxy.yml up -d --build
 docker system prune -f
+
+
+# register services to kong
+bash ./bin/kong-register-api-backend.sh
