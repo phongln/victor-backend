e086f1aec29060cfb0538f3f4861fe45029dc06b
vinhpham <phamtanvinh.me@gmail.com>
2019-09-14 22:49:57 +0700

    [2019-09-14 22:49] Vinhs-Mac.local vinhpham: fucking consul

--- nginx/service.consul-template.sh
+++ nginx/service.consul-template.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 exec consul-template \
-    -consul-addr consul:8500 \
-    -template "/etc/consul-templates/nginx.default.conf:/etc/nginx/conf.d/default.conf:service nginx reload" \
+    -consul-addr $CONSUL_HOST:8500 \
+    -template "/etc/consul-templates/nginx.default.conf:/etc/nginx/conf.d/default.conf:nginx -s reload" \
     -template "/etc/consul-templates/nginx.index.html:/usr/share/nginx/html/index.html"
