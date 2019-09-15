14466e621306464338cb8f5c10d3192f06732988
vinhpham <phamtanvinh.me@gmail.com>
2019-09-16 00:22:27 +0700

    [2019-09-16 00:22] Vinhs-Mac.local vinhpham: clean up

--- start-all.sh
+++ start-all.sh
@@ -6,12 +6,11 @@ DOCKER_COMPOSE_DIR='./docker-compose'
 bash ./init-env-file.sh
 bash ./init-docker-resource.sh
 
-for _compose_file in $DOCKER_COMPOSE_DIR/[^_]*; do
+for _compose_file in $DOCKER_COMPOSE_DIR/[^_]*.yml; do
     docker-compose -p $PROJECT_NAME -f $_compose_file up -d --build
 done
 
-docker system prune -f
-
-
 # register services to kong
 bash ./bin/register-api/_all.sh
+
+docker system prune -f
